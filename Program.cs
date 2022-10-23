0c3d0a544bf1a138082a8dc831effa454c47cfd7
Elbek <e.normurodov@mail.ru>
2022-10-22 21:14:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CalculatR.Classes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Security security = new Security();
+        Calculator calculator = new Calculator();
+
+        security.CheckPassword();
+        calculator.GetInputs();
+
+        string message =
+            !(calculator.IsFirstNumberPositive())
+                ? "1st number is not positive"
+                : "1st number is not negative";
+        Console.WriteLine(message);
+
+
+        calculator.CompareInputs();
+
+        string result = calculator.Calculate();
+
+        Console.WriteLine(result);
+        calculator.PrintEvenNumbers();
+        calculator.PrintMultiplicationTable();
+    }
+}
